58cb7c638a2b601405a7038797415449bf47a9f0
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-06-27 16:53:45 +0200

    Added Google authentication.

--- Authentification/google-auth.php
+++ Authentification/google-auth.php
@@ -3,9 +3,9 @@ session_start();
 
 $CLIENT_ID = '994112343166-ed7ptuk74kdqmhvfrgm1p7bv7i476vgb.apps.googleusercontent.com';
 $CLIENT_SECRET = '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';
-$redirect_uri = 'http://localhost:8888/Boutique-en-ligne/Authentification/google-auth.php'; // Assurez-vous que c'est exactement le même que celui configuré dans la Console Google Cloud
+$redirect_uri = 'http://localhost:8888/Boutique-en-ligne/Authentification/google-auth.php';
 
-$code = $_POST['code']; // Assurez-vous que vous recevez correctement le code d'autorisation depuis Google OAuth
+$code = $_POST['code'];
 
 $url = 'https://oauth2.googleapis.com/token';
 $data = array(
@@ -34,13 +34,12 @@ if ($response === false) {
 $token_data = json_decode($response, true);
 
 if (isset($token_data['access_token'])) {
-    // Succès : vous avez obtenu le jeton d'accès
     $_SESSION['access_token'] = $token_data['access_token'];
-    $_SESSION['refresh_token'] = $token_data['refresh_token']; // Optionnel
-    header('Location: ../Boutique/Boutique.html'); // Rediriger vers une autre page après l'authentification réussie
+    $_SESSION['refresh_token'] = $token_data['refresh_token'];
+    header('Location: ../Boutique/Boutique.html');
     exit();
 } else {
-    // Erreur lors de l'obtention du jeton d'accès
+
     die('Erreur lors de l\'échange du code d\'autorisation');
 }
 ?>
